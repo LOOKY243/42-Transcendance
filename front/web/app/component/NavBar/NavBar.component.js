80758918883387bb34c74dedde864b8c29b0147b
Kurt Butor-Blamont <kbutor-b@1C7.42angouleme.fr>
2024-08-22 15:14:30 +0200

    for merge

--- front/web/app/component/NavBar/NavBar.component.js
+++ front/web/app/component/NavBar/NavBar.component.js
@@ -12,37 +12,33 @@ export class NavBarComponent extends AComponent {
 
 	generateHtml(config) {
 		this.html = `
-    		<div class="container-fluid">
-        		<a class="navbar-brand" href="#">Transcendence</a>
-        		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
-                	aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            		<span class="navbar-toggler-icon"></span>
-       			</button>
-        		<div class="collapse navbar-collapse" id="navbarNavDropdown">
-          			<ul class="navbar-nav me-auto">
-                		<li class="nav-item ">
-                    		<a class="nav-link nav-text" aria-current="page" href="#">ft_pong</a>
-                		</li>
-                		<li class="nav-item">
-                    		<a class="nav-link nav-text" href="#">Live chat</a>
-                		</li>
-            		</ul>
-            		<button type="button" class="btn btn-outline-primary me-3">Sign in</button>
-            		<button type="button" class="btn btn-outline-secondary">Register</button>
-       		 	</div>
-    		</div>`
-	}
-}
-
-/*<nav class="navbar navbar-expand-lg bg-body-tertiary fixed-top">
 			<div class="container-fluid">
-       			<a class="navbar-brand" href="/">Home</a>
+				<a class="navbar-brand me-4" href="/">
+					<img class="nav-img" src="app/assets/img/homeIcon.png" alt="HomeIcon" width="48" height="48">
+				</a>
         		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
 					aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             		<span class="navbar-toggler-icon"></span>
         		</button>
         		<div class="collapse navbar-collapse" id="navbarNavDropdown">
-            		<button type="button" class="btn btn-outline-primary me-3">profile</button>
+					<div class="navbarCollapse d-flex justify-content-between">
+						<a href="/chat">
+							<img class="nav-img" src="app/assets/img/chatIcon.png" alt="chatIcon" width="48" height="48">
+						</a>
+					</div>
+					<div class="btn-group">
+						<button class="btn btn-sm" type="button">Lang</button>
+						<button type="button" class="btn btn-sm dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
+							<span class="visually-hidden">Toggle Dropdown</span>
+						</button>
+						<ul class="dropdown-menu">
+							...
+						</ul>
+					<a class="me-2" href="/profile">
+						<img class="nav-img" src="app/assets/img/profileIcon.png" alt="profileIcon" width="48" height="48">
+					</a>
+					</div>
         		</div>
-			</div>
-		</nav>`*/
+			</div>`
+	}
+}
