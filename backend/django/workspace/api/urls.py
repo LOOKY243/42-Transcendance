930459833e68cad3857b8acd7664b0eacf3dd3b7
LOOKY <gad.marre@gmail.com>
2024-09-27 13:12:04 +0200

    pull from main after merge

--- backend/django/workspace/api/urls.py
+++ backend/django/workspace/api/urls.py
@@ -18,8 +18,6 @@ from .views import (
     GetProfilePictureView
 )
 
-# TwoFactorSetupView, TwoFactorVerifyView
-
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
--- backend/django/workspace/ft_django/settings.py
+++ backend/django/workspace/ft_django/settings.py
@@ -58,7 +58,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
-    # 'django_otp.middleware.OTPMiddleware',
 ]
 
 CORS_ALLOWED_ORIGINS = [
